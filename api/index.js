82770011d0c39650cdd26f4a74353aadf344ed61
jatin1234567890 <jatinjainjatin077@gmail.com>
2024-08-01 20:05:58 +0530

    controller and routes

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
-import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import db from './config/database.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 
 const app=express();
 dotenv.config();
 
-// connect to db
 db.connect();
+
+app.use(express.json());
 
 app.listen(3000,() => {
     console.log("Server is running at 3000!");
 })
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 
 
 
